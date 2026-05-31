90287d16ba76bc0c85e4ddd9f62702d52d174c14
Kudigotdis <kudigotdis@github.com>
2026-05-31 14:50:30 +0200

    fix: update Firebase config to wirog-app-93318 after app rename

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBA9tlLWFrizPSbg7DGWz1ojmxNG9UbldI",
-  authDomain: "foromane-app.firebaseapp.com",
-  projectId: "foromane-app",
-  storageBucket: "foromane-app.firebasestorage.app",
+  authDomain: "wirog-app-93318.firebaseapp.com",
+  projectId: "wirog-app-93318",
+  storageBucket: "wirog-app-93318.firebasestorage.app",
   messagingSenderId: "86539667419",
   appId: "1:86539667419:web:d6075cbc67b4e1e77389fe",
   measurementId: "G-V94C6P143F"
